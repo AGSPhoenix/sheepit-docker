2bcb1320114308004c5d9dbf79b44a26f5f0bcc9
AGSPhoenix <lx45803@gmail.com>
2016-04-10 16:20:54 -0400

    Add status reports, clean up console output

--- startrenderer.sh
+++ startrenderer.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
 #Check for updates
-latestVersion=`curl -I https://www.sheepit-renderfarm.com/media/applet/client-latest.php|grep -Po 'Content-Disposition:.*filename="?\Ksheepit-client-[\d\.]+'`
+echo Checking for client updates...
+latestVersion=`curl --silent --head https://www.sheepit-renderfarm.com/media/applet/client-latest.php|grep -Po 'Content-Disposition:.*filename="?\Ksheepit-client-[\d\.]+'`
 
 if [ ! -e $latestVersion.jar ]
     then
-    rm sheepit-client*.jar
+    echo Updating client...
+    rm -f sheepit-client*.jar
     #Download new client. Can't just use curl -OJ due to malformed Content-Disposition header supplied by client-latest.php
     curl https://www.sheepit-renderfarm.com/media/applet/client-latest.php > $latestVersion.jar
 fi
 
+echo Starting client.
 java -jar /sheep/$latestVersion.jar -ui text -login $user_name -password $user_password -cores $cpu -cache-dir /sheep/cache
